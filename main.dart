e714928f05bd4fe2dbaa2c1fdb0fd00537987eb7
emanuel <emanuelderrossif@gmail.com>
2026-07-17 21:18:12 -0300

    blablabla

--- main.dart
+++ main.dart
@@ -2,7 +2,7 @@ void main() {
   final mensagens = <String>[
     'Bem-vindas e bem-vindos ao projeto!',
     // ===== EDITE A LINHA ABAIXO (a dupla toda edita ESTA mesma linha) =====
-    'vai corinthians porra',
+    'fogo no puteiro',
     // =====================================================================
     'Bons commits para todos.',
   ];
